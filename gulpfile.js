d809fa9e5ad50405a9ba0a8d0e0539b4d1404ef5
Angelica Maria Duque <mduque@hugeinc.com>
2018-06-28 14:55:19 -0500

    added fonts, updated gulp file and styles scss

--- gulpfile.js
+++ gulpfile.js
@@ -5,29 +5,29 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
-
-gulp.task('sass', function(){
-  return gulp.src('src/css/style.scss')
+gulp.task('sass', function() {
+  return gulp
+    .src('src/Styles/index.scss')
     .pipe(sass()) // Using gulp-sass
     .pipe(gulp.dest('src/css'))
-    .pipe(browserSync.reload({stream:true}))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
 gulp.task('browserSync', function() {
   browserSync.init({
     server: {
       baseDir: 'src'
-    },
-  })
-})
+    }
+  });
+});
 
 // Gulp watch syntax
 
 // gulp.watch('src/css/**/*.scss', ['sass']);
 
-gulp.task('watch', ['browserSync','sass'], function(){
+gulp.task('watch', ['browserSync', 'sass'], function() {
   gulp.watch('src/css/style.scss', ['sass']);
   gulp.watch('src/*.html', browserSync.reload);
-  gulp.watch('src/js/*.js', browserSync.reload);
+  // gulp.watch('src/js/*.js', browserSync.reload);
   // Other watchers
-})
+});
